a379957aff6c98f258bd66bcf028d94e2bf12dbe
Lucky <luckyshaif888@gmail.com>
2022-10-13 16:58:20 +0530

    Phase 2 put api validation done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,18 +1,87 @@
 const productModel = require("../models/productModel");
 const { uploadFile } = require("../aws Config/awsConfig");
 const mongoose = require("mongoose");
+const {
+  isValidName,
+  isValidPhone,
+  isValidEmail,
+  isValidPassword,
+  isValidRequestBody,
+  isValid,
+  isvalidPincode,
+  isValidObjectId,
+} = require("../validator/validator");
 
 //_____________________________Create product ___________________________________
 const createProduct = async function (req, res) {
   try {
-    if (Object.keys(req.body).length == 0) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please provide details" });
+
+    let data = req.body
+
+    let { title, description, price, currencyId, currencyFormat } = { ...data }
+
+    if (Object.keys(data).length == 0) {
+      return res.status(400).send({ status: false, msg: "Please provide details" });
     }
 
-    let availableSizes = JSON.parse(req.body.availableSizes);
+    let files = req.files;
 
+    if (!title)
+      return res.status(400).send({ status: false, msg: "title is required" });
+    if (!description)
+      return res.status(400).send({ status: false, msg: "description is required" });
+    if (!price)
+      return res.status(400).send({ status: false, msg: "price is required" });
+    if (!currencyId)
+      return res.status(400).send({ status: false, msg: "currencyId is required" });
+    if (!currencyFormat)
+      return res.status(400).send({ status: false, msg: "currencyFormat is required" });
+
+    if (files.length === 0)
+      return res.status(400).send({ status: false, msg: "productImage is required" });
+
+    if (!req.body.availableSizes)
+      return res.status(400).send({ status: false, msg: "availableSizes is required" });
+
+    let availableSizes = JSON.parse(req.body.availableSizes);
+    data.availableSizes = availableSizes
+
+    if (Array.isArray(availableSizes) && availableSizes.length > 0) {
+      for (let i = 0; i < availableSizes.length; i++) {
+        const element = availableSizes[i];
+
+        if (!["S", "XS", "M", "X", "L", "XXL", "XL"].includes(element)) {
+          return res.status(400).send({
+            status: false,
+            message: `available sizes must be from:  S, XS, M, X, L, XXL, XL`,
+          });
+        }
+      }
+      // availableSizes = { $in: availableSizes };
+    } else {
+      return res.status(400).send({
+        status: false,
+        message: "size should be in array format: [X, M,L]",
+      });
+    }
+
+    if (!isValidName(title))
+      return res.status(400).send({ status: false, msg: "Invalid title" });
+
+    const isTitleAlreadyUsed = await productModel.findOne({
+      title: req.body.title,
+    });
+    if (isTitleAlreadyUsed)
+      return res.status(404).send({ status: false, msg: "Title is already used" });
+
+    if (!isValidName(description))
+      return res.status(400).send({ status: false, msg: "Invalid description" });
+
+    if (currencyId !== "INR")
+      return res.status(400).send({ status: false, msg: "crrencyId must be INR" })
+
+    if (currencyFormat !== "₹")
+      return res.status(400).send({ status: false, msg: "currencyFormat must be in ₹" })
 
     let uploadedFileURL;
 
@@ -21,21 +90,9 @@ const createProduct = async function (req, res) {
     } else {
       res.status(400).send({ msg: "No file found" });
     }
+    data.productImage = uploadedFileURL
 
-    let createUserData = {
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      currencyId: req.body.currencyId,
-      currencyFormat: req.body.currencyFormat,
-      isFreeShipping: req.body.isFreeShipping,
-      productImage: uploadedFileURL,
-      style: req.body.style,
-      availableSizes: availableSizes,
-      installments: req.body.installments,
-    };
-
-    const savedData = await productModel.create(createUserData);
+    const savedData = await productModel.create(data);
     return res.status(201).send({ status: true, msg: savedData });
   } catch (error) {
     return res.status(500).send({ status: false, error: error.message });
@@ -154,14 +211,34 @@ const updateProduct = async function (req, res) {
     let uploadedFileURL;
     let productId = req.params.productId;
 
+    let details = req.body
+
+    let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, installments } = { ...details }
+
     if (!mongoose.isValidObjectId(productId))
-      return res
-        .status(400)
-        .send({ status: false, message: "Ivalid productId" });
+      return res.status(400).send({ status: false, message: "Ivalid productId" });
+
+    //   if (req.body.currencyId && req.body.currencyId !== "INR")
+    // return res.status(400).send({ status: false, msg: "crrencyId must be INR" })
+
+    if (currencyId)
+      return res.status(400).send({ status: false, msg: "You can't change currencyId" })
+
+    if (currencyFormat)
+      return res.status(400).send({ status: false, msg: "You can't change currencyFormat" })
+
+    // if (req.body.currencyFormat && req.body.currencyFormat !== "₹")
+    // return res.status(400).send({ status: false, msg: "currencyFormat must be in ₹" })
+
+    if (title && !isValidName(title))
+      return res.status(400).send({ status: false, msg: "Invalid title" });
+
+    if (description && !isValidName(description))
+      return res.status(400).send({ status: false, msg: "Invalid description" });
 
     let availableSizes;
-    if (req.body.availableSizes) {
-      availableSizes = JSON.parse(req.body.availableSizes);
+    if (details.availableSizes) {
+      availableSizes = JSON.parse(details.availableSizes);
     }
 
     if (req.files && req.files.length > 0) {
@@ -169,33 +246,20 @@ const updateProduct = async function (req, res) {
     }
 
     let updatedProduct = await productModel.findOneAndUpdate(
-      { _id: productId, isDeleted: false },
+      { _id: productId, isDeleted: false }, {
+      $set:
       {
-        $set: {
-          title: req.body.title,
-          description: req.body.description,
-          price: req.body.price,
-          currencyId: req.body.currencyId,
-          currencyFormat: req.body.currencyFormat,
-          isFreeShipping: req.body.isFreeShipping,
-          productImage: uploadedFileURL,
-          style: req.body.style,
-          availableSizes: availableSizes,
-          installments: req.body.installments,
-        },
+        title: title, description: description, price: price, isFreeShipping: isFreeShipping, productImage: uploadedFileURL,
+        style: style, availableSizes: availableSizes, installments: installments,
       },
-      { new: true }
+    }, { new: true }
     );
 
-    if(!updatedProduct){
-      return res.status(404).send({status:false,msg: "Product not found"})
+    if (!updatedProduct) {
+      return res.status(404).send({ status: false, msg: "Product not found" })
     }
 
-    return res.status(200).send({
-      status: true,
-      message: "Product details updated",
-      data: updatedProduct,
-    });
+    return res.status(200).send({ status: true, message: "Product details updated", data: updatedProduct });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
